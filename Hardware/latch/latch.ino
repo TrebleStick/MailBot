2ddc4b13a8666d6db961f97aca0c948844f51f56
TrebleSick <jcp115@ic.ac.uk>
2018-11-09 11:41:47 +0000

    Latch code base working

--- Hardware/latch/latch.ino
+++ Hardware/latch/latch.ino
@@ -1,19 +1,22 @@
 // global variables
-int baud_rate = 57600;
-int latch_delay_ms = 250;
+int baud_rate = 9600;
+int latch_delay_ms = 2000;
+int pin = 0;
 // --------------------------------------------//
 //-------------global functions---------------//
 
 //---------------------------------------------//
 void setup(){
   //set 7 digital output pins.
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(1, OUTPUT); //brown
+  pinMode(2, OUTPUT); //orange
+  pinMode(3, OUTPUT); //grey
+  pinMode(4, OUTPUT); //red
+  pinMode(5, OUTPUT); //pink
+  pinMode(6, OUTPUT); //blue
+  pinMode(7, OUTPUT); //green
+
+  pinMode(9, OUTPUT); //RESET
 
 
   Serial.begin(baud_rate);
@@ -26,14 +29,31 @@ void setup(){
 //-------------------MAIN-------------------//
 void loop(){
 
-  if( Serial.available() ){
-    int pin = Serial.read();
+  if( Serial.available() > 0){
+    // pin = Serial.read() - 48; // remove ASCII encoding
+    pin = Serial.parseInt();
     //might need some pre processing here etc
-    //build in a reset code for example.
 
-    digitalWrite(pin, HIGH);
-    delay(latch_delay_ms);
-    digitalWrite(pin, LOW);
+    //Reset device cmd
+    if(pin == 9){
+      digitalWrite(9, HIGH);
+    }
+    else if(pin != 0){
+      digitalWrite(9, HIGH);
+      digitalWrite(pin, HIGH);
+      delay(latch_delay_ms);
+      digitalWrite(pin, LOW);
+      digitalWrite(9, LOW);
+    }
+
+    // Serial.write(pin);
+
+    Serial.println(pin, HEX);
   }
+
+  // digitalWrite(3, HIGH);
+  // delay(500);
+  // digitalWrite(3, LOW);
+  // delay(500);
 
 }
