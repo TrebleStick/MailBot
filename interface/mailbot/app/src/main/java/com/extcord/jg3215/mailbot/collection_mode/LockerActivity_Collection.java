9c1f1c1788174d82d6c6eabd5521d4196640607f
Javier Geis <jg3215@ic.ac.uk>
2018-11-26 15:36:43 +0000

    Added UsbService, gradle issues

--- interface/mailbot/app/src/main/java/com/extcord/jg3215/mailbot/collection_mode/LockerActivity_Collection.java
+++ interface/mailbot/app/src/main/java/com/extcord/jg3215/mailbot/collection_mode/LockerActivity_Collection.java
@@ -12,6 +12,8 @@ import com.extcord.jg3215.mailbot.LockerManager;
 import com.extcord.jg3215.mailbot.PackageData;
 import com.extcord.jg3215.mailbot.R;
 
+import java.util.concurrent.locks.Lock;
+
 public class LockerActivity_Collection extends AppCompatActivity {
 
     // TODO: Send a PIN code for each package to the computer as the mail item is locked away
@@ -43,6 +45,8 @@ public class LockerActivity_Collection extends AppCompatActivity {
         // Get data from previous activity stored in a bundle
         Bundle detailActivityData = this.getIntent().getExtras();
 
+
+
         mLockerManager = new LockerManager(this);
 
         if (detailActivityData != null) {
@@ -57,6 +61,11 @@ public class LockerActivity_Collection extends AppCompatActivity {
 
             lockerIndex = detailActivityData.getInt("lockerTag");
             Log.i(TAG, "Locker to open = " + String.valueOf(lockerIndex + 1));
+
+            //Set the opened locker number in speech bubble in LockerActivity_Collection
+            TextView LockerText = (TextView) findViewById(R.id.textLockerOpened);
+            LockerText.setText("I have opened locker " + String.valueOf(lockerIndex + 1) + " for your mail item! Please store it there so I can deliver it, and close the locker before continuing.");
+
         } else {
             // throw some exception/error -> there should be data from the previous activity
             Log.i(TAG, "No data sent from previous activity");
